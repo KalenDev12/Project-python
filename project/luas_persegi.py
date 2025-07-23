dbfeda93577ef4dfd9d4e0f758775561456eadca
Kalen Dev <kitpro245@gmail.com>
2025-07-23 09:32:13 +0700

    First Commit

---
+++ project/luas_persegi.py
@@ -0,0 +1,16 @@
+##cara menghitung luas sisi persegi menggunakan bahasa pemrograman python 
+
+##pertanyaan:
+#1.Buatkan saya program cara menghitung luas sisi persegi!
+
+##Hasilnya
+
+print("=========Hitung sisi persegi=============")
+Luas = float(input("Berapa luas sisi dari persegi? :"))
+hasil = Luas * Luas
+
+print(f"Hasil dari sisi persegi adalah {hasil}")
+
+
+
+
---
+++ project/variabel.str.py
@@ -0,0 +1,17 @@
+##projek pertama belajar menggunakan bahasa pemrograman python 
+
+##cara menggunakan variable input string (str)
+#perintah:
+#1.tuliskan nama kamu beserta kelas dalam format bahasa pemrograman 
+#2.lalu ubah menjadi output string
+
+##Hasilnya......
+print("=======input variabel string (str)========")
+nama = str(input("masukkan namamu :"))
+kelas = str(input("kelas berapa kamu :"))
+jurusan = str(input("Kamu dari Jurusan apa? :"))
+
+print("Nama Kamu Adalah",nama,", Dari Kelas",kelas,jurusan)
+
+
+
